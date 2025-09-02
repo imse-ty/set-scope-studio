13c73404e2a423851e948952d5f5014831923269
Imsety <imsety@imsety.com>
2025-09-02 00:21:29 -0400

    Add project fields

--- schemaTypes/project.ts
+++ schemaTypes/project.ts
@@ -25,6 +25,80 @@ export default defineType({
       type: 'text',
       rows: 5,
     }),
+    defineField({
+      name: 'videoUrl',
+      title: 'Video URL',
+      type: 'url',
+    }),
+    defineField({
+      name: 'stats',
+      title: 'Stats',
+      type: 'array',
+      of: [
+        {
+          name: 'statItem',
+          title: 'Stat Item',
+          type: 'object',
+          fields: [
+            defineField({name: 'prefix', title: 'Prefix', type: 'string'}),
+            defineField({name: 'stat', title: 'Stat', type: 'string'}), // or 'number'
+            defineField({name: 'suffix', title: 'Suffix', type: 'string'}),
+          ],
+          preview: {
+            select: {prefix: 'prefix', stat: 'stat', suffix: 'suffix'},
+            prepare: ({prefix, stat, suffix}) => ({
+              title: `${prefix ?? ''}${stat ?? ''}${suffix ?? ''}`.trim(),
+            }),
+          },
+        },
+      ],
+    }),
+    defineField({
+      name: 'info',
+      title: 'Info items',
+      type: 'array',
+      of: [
+        defineField({
+          name: 'item',
+          title: 'Info item',
+          type: 'object',
+          fields: [
+            defineField({
+              name: 'label',
+              title: 'Label',
+              type: 'string',
+            }),
+            defineField({
+              name: 'text',
+              title: 'Text',
+              type: 'string',
+            }),
+          ],
+          preview: {
+            select: {
+              title: 'label',
+              subtitle: 'text',
+            },
+          },
+        }),
+      ],
+    }),
+    defineField({
+      name: 'cover',
+      title: 'Cover',
+      type: 'image',
+      fields: [
+        {
+          title: 'Alternative text',
+          name: 'alt',
+          type: 'string',
+          validation: (Rule) => Rule.required(),
+          options: {
+            isHighlighted: true,
+          },
+        },
+      ],
+    }),
   ],
 
   preview: {
